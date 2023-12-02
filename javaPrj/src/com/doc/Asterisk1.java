f0d6536707eb789a0448d109a0dee6c66c2986e3
withgrow <icandogo1@naver.com>
2023-12-02 14:14:02 +0900

    업데이트

--- javaPrj/src/com/doc/Asterisk1.java
+++ javaPrj/src/com/doc/Asterisk1.java
@@ -5,18 +5,7 @@ public class Asterisk1 {
 	public static void main(String[] args) {
 //		// TODO Auto-generated method stub
 //
-//		for(int i=0; i<5; i++) {
-//			for(int j=0; j<4-i; j++ ) {
-//				System.out.print(" ");
-//			}
-//			for(int k=0; k<2*i+1; k++) {
-//				System.out.print("*");
-//			}
-//			System.out.println();
-//			
-//			
-//			
-//		}   //3번째 피라미드
+
 	
 		
 		
---
+++ javaPrj/src/com/doc/Asterisk2.java
@@ -0,0 +1,19 @@
+package com.doc;
+
+public class Asterisk2 {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 4 - i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k < 2 * i + 1; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+
+		}
+	}
+
+}
