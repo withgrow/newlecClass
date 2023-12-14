5159e1970c875b395127ed1dcc7eaec0b0198389
hi <hi@DESKTOP-DP3RJ5I>
2023-12-14 14:39:02 +0900

    검색기능추가

--- FigmaPrj/src/AHAH.java
+++ FigmaPrj/src/AHAH.java
@@ -26,16 +26,14 @@ public class AHAH {
 			fis.close();
 
 		}
-		
-			
-		
+
 		int i = 0;
 		FileInputStream fis = new FileInputStream("C:\\Users\\oneto\\git\\newlecClass\\FigmaPrj\\src\\res\\a2.txt");
 		Scanner scan = new Scanner(fis);
 
 		String[] str = new String[countBoard + 50];
 		{
-			
+
 			while (scan.hasNextLine()) {
 
 				str[i] = scan.nextLine();
@@ -43,66 +41,91 @@ public class AHAH {
 			}
 
 		}
-		
+
 		{// 게시판 파일 로드해서 새로운 파일에 쓰기
 
-			
-
-			FileOutputStream fos = new FileOutputStream("C:\\Users\\oneto\\git\\newlecClass\\FigmaPrj\\src\\res\\a2.txt");
+			FileOutputStream fos = new FileOutputStream(
+					"C:\\Users\\oneto\\git\\newlecClass\\FigmaPrj\\src\\res\\a2.txt");
 			PrintStream ps = new PrintStream(fos);
-	
-			
-			Scanner scan2 = new Scanner(System.in);  //사용자입력
-			boolean a = true;
-			int t=1;
-			while (a) {
-				
-				String nickname;
-				do {
-					
-					System.out.println("닉네임 입력");
-					nickname = scan2.nextLine()+",";
-					str[i] = nickname;
-					
-					if(nickname.equals("q1")) {
-						break;
-					}
-				} while (nickname.equals("") || nickname == null);
 
-			
-				System.out.println("제목입력");
-				str[i]+= scan2.nextLine()+",";
-				
-				
-				System.out.println("내용입력");
-				str[i] += scan2.nextLine();
-				countBoard++;
-				i++;
-				if(t==1) {
-				for (int y = 0; y < i; y++) {
-				    ps.printf("%s\n",str[y]);
-				    
-			
-				t++;
-				}
-				countBoard++;
-			continue;
-				}
-			
-				
-					for (int x = countBoard-1; x < countBoard; x++) {
-					
-					
-					    ps.printf("%s\n",str[x]);
-					
-				
+			Scanner scan2 = new Scanner(System.in); // 사용자입력
+			int input = scan2.nextInt();
+			switch (input) {
+			case 1: {
+
+				boolean a = true;
+				int t = 1;
+				while (a) {
+
+					String nickname;
+					do {
+
+						System.out.println("닉네임 입력");
+						nickname = scan2.nextLine() + ",";
+						str[i] = nickname;
+
+						if (nickname.equals("q1")) {
+							break;
+						}
+					} while (nickname.equals("") || nickname == null);
+
+					System.out.println("제목입력");
+					str[i] += scan2.nextLine() + ",";
+
+					System.out.println("내용입력");
+					str[i] += scan2.nextLine();
+					countBoard++;
+					i++;
+					if (t == 1) {
+						for (int y = 0; y < i; y++) {
+							ps.printf("%s\n", str[y]);
+
+							t++;
+						}
+						countBoard++;
+						continue;
 					}
-				
-				
+
+					for (int x = countBoard - 1; x < countBoard; x++) {
+
+						ps.printf("%s\n", str[x]);
+
+					}
+
+					ps.close();
+					fos.close();
+				}
+				break;
 			}
-		
-		
-		
+
+			case 2:
+
+			{//계시판 검색
+			FileInputStream fis3 = new FileInputStream("C:\\Users\\hi\\git\\newlecClass\\FigmaPrj\\src\\res\\board.csv");
+			Scanner scan3 = new Scanner(fis);
+			scan.nextLine();
+			Scanner scan4 = new Scanner(System.in);
+			
+			
+			System.out.println("제목을 검색하세요 ");
+			String findText = scan2.nextLine();
+			
+			while(scan.hasNextLine()) {
+				String text = scan.nextLine();
+				
+				String[] arr = text.split(",");
+				
+				if(arr[2].contains(findText)) {
+					System.out.println("제목:"+arr[2]);
+					System.out.println("내용:"+arr[3]);
+				}	
+			}
+			
+			fis.close();
 		}
-		
-}}
+
+			}
+		}
+
+	}
+}
